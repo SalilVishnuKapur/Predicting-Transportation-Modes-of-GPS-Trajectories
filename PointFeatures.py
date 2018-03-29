ee6bd5ca534fabb0b85c69c71ddf384120fe34bc
SalilVishnuKapur <salilvishnukapur@gmail.com>
2018-03-28 23:33:01 -0300

    Added code for doing predictions

--- PointFeatures.py
+++ PointFeatures.py
@@ -342,4 +342,78 @@ def fitHierarchyDTC(trainData,trainLabels, modelDic):
                                     labels5 = relabel(5,trainLabels5)
                                     C5 = DecisionTreeClassifier().fit(trainData5, labels5)
                                     modelDic['C5'] = C5
-    return modelDic                
+    return modelDic
+
+
+# This is the implementation of the predict method where you pass your learnt model and it gives you the predicted labels.
+def predictHierarchy(testData, modelDic):
+    testData1 = testData.copy()
+    indexList = []
+    predList = []
+    frames = []
+    predLabels = []
+    pred = modelDic['C1'].predict(testData1)
+    testData1['newLabels'] = pred
+    grp1 = testData1.groupby('newLabels')
+    for grp in grp1:
+        if(grp[0] == -100):
+            testData2 = grp[1].iloc[:,0:20]
+            pred2 = modelDic['C2'].predict(testData2)
+            testData2['newLabels'] = pred2
+            grp2 = testData2.groupby('newLabels')
+            for grp in grp2:
+                #print('grp2 ->'+ str(grp[0]))
+                if(grp[0] == 80):
+                    testData3 = grp[1].iloc[:,0:20]
+                    pred3 = modelDic['C3'].predict(testData3)
+                    testData3['newLabels'] = pred3
+                    grp3 = testData3.groupby('newLabels')
+                    for grp in grp3:
+                        #print('grp3 ->'+ str(grp[0]))
+                        if(grp[0] == 60):
+                            testData4 = grp[1].iloc[:,0:20]
+                            pred4 = modelDic['C4'].predict(testData4)
+                            testData4['newLabels'] = pred4
+                            grp4 = testData4.groupby('newLabels')
+                            for grp in grp4:
+                                #print('grp4 ->'+ str(grp[0]))
+                                if(grp[0] == 40):
+                                    testData5 = grp[1].iloc[:,0:20]
+                                    pred5 = modelDic['C5'].predict(testData5)
+                                    testData5['newLabels'] = pred5
+                                    grp5 = testData5.groupby('newLabels')
+                                    for grp in grp5:
+                                        #print('grp5 ->'+ str(grp[0]))
+                                        if(grp[0] == 20):
+                                            predList.append(grp[1].iloc[:,20])
+                                        if(grp[0] == -20):
+                                            predList.append(grp[1].iloc[:,20])    
+                                if(grp[0] == -40):
+                                    predList.append(grp[1].iloc[:,20])    
+                        if(grp[0] == -60):
+                            predList.append(grp[1].iloc[:,20])    
+                if(grp[0] == -80):
+                    predList.append(grp[1].iloc[:,20])            
+        if(grp[0] == 100):  
+            predList.append(grp[1].iloc[:,20]) 
+    # Converting the predictions numberical value to corresponding class value i.e {100 -> 'train', -80 -> 'subway',
+    # -60 -> 'walk', -40 -> 'car', -20 -> 'taxi', 20 -> 'bus'}if output of hierarchy was 100 then get 'train'. Similarliy
+    # for other numerical values to their respective classes.
+    for i in range(len(predList)):
+        frames.append(pd.DataFrame(predList[i]))
+    result = pd.concat(frames)
+    predictions = result.sort_index(axis=0, ascending=True)
+    for i in predictions['newLabels']:
+        if(i==100):
+            predLabels.append('train')
+        if(i==-80):
+            predLabels.append('subway')
+        elif(i==-60):
+            predLabels.append('walk')
+        elif(i==-40):
+            predLabels.append('car')
+        elif(i==-20):
+            predLabels.append('taxi')
+        elif(i==20):
+            predLabels.append('bus')
+    return (predLabels)                
